0ac126dac118f6f1f9ad8f31337de0b58903a417
Ostap Shcherbii <shchostap@gmail.com>
2021-11-10 14:51:04 +0200

    lab-6

---
+++ model.py
@@ -0,0 +1,73 @@
+import os
+import sys
+
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    DECIMAL,
+    ForeignKey,
+)
+
+from sqlalchemy import orm, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session, relationship
+
+
+
+DB_URL = "mysql+mysqlconnector://root:password@localhost/baza_pp"
+
+engine = create_engine(DB_URL)
+
+SessionFactory = sessionmaker(bind=engine)
+
+Session = scoped_session(SessionFactory)
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "User"
+
+    UserName = Column(String(length=45), primary_key=True, unique=True)
+    firstName = Column(String(length=45), nullable=False)
+    lastName = Column(String(length=45), nullable=False)
+    email = Column(String(length=45), nullable=False, unique=True)
+    phone = Column(String(length=45), nullable=False, unique=True)
+    password = Column(String(length=45), nullable=False)
+
+    def __str__(self):
+        return f"User ID : {self.UserName}\n" \
+               f"firstName : {self.firstName}\n" \
+               f"lastName : {self.lastName}\n" \
+               f"email : {self.email}\n" \
+               f"phone : {self.phone}\n" \
+               f"password : {self.password}\n"
+
+
+class Account(Base):
+    __tablename__ = "Account"
+
+    AccountNumber = Column(Integer, primary_key=True, unique=True)
+    balance = Column(DECIMAL(10, 2), nullable=False)
+    currencyCode = Column(String(length=45), nullable=False)
+    UserName = Column(String(length=45), ForeignKey("User.UserName"), nullable=False)
+
+    user = relationship("User")
+
+
+class Transfer(Base):
+    __tablename__ = "Transfer"
+    idTransfer = Column(Integer, primary_key=True, unique=True)
+    amount = Column(DECIMAL(10, 2), nullable=False)
+    currencyCode = Column(String(length=45), nullable=False)
+    fromAccountNumber = Column(Integer, ForeignKey("Account.AccountNumber"), nullable=False)
+    toAccountNumber = Column(Integer, ForeignKey("Account.AccountNumber"), nullable=False)
+
+    # Account = orm.relationship("Account")
+    Account = orm.relationship("Account", foreign_keys=[fromAccountNumber])
+    Account = orm.relationship("Account", foreign_keys=[toAccountNumber])
+
+
+# alembic revision --autogenerate -m "First"
+# alembic upgrade head
